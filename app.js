15a3f2e307d6f7e23ee109247dd81cc900dc4e89
Shubham kumar <93025079+shubhamkhalari2003@users.noreply.github.com>
2023-05-18 01:01:21 +0530

    second commit

--- app.js
+++ app.js
@@ -2,7 +2,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose")
 const _ = require("lodash");
+
+
+mongoose.connect("mongodb://localhost:27017/blogDB",{useNewUrlParser: true})
+
+//PostSchema
+
+const postSchema = {
+  title : String,
+  content : String
+}
+
+//model
+const Post = mongoose.model("Post", postSchema);
+
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -15,15 +30,20 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const posts = [];
-
-app.get("/", function(req,res){
-  res.render("home",
-  {
-    startingContent : homeStartingContent,
-    blogPosts : posts
-  });
+app.get("/", function(req, res) {
+  Post.find({})
+    .then(function(posts) {
+      res.render("home", {
+        startingContent: homeStartingContent,
+        blogPosts: posts
+      });
+    })
+    .catch(function(err) {
+      console.log(err);
+      // res.sendStatus(500); // or render an error page
+    });
 });
+
 
 app.get("/about", function(req,res){
   res.render("about",
@@ -43,29 +63,51 @@ app.get("/compose", function(req,res){
   res.render("compose");
 });
 
-app.get("/posts/:postName", function(req,res){
-  // console.log(req.params.postName);
 
-  posts.forEach(function(post){
-    if(_.lowerCase(req.params.postName) === _.lowerCase(post.title)){
-      res.render("post",{
-        postTitle : post.title,
-        postBody : post.content
-      })
+app.get("/posts/:postName", function(req, res) {
+  Post.find({}).then(function(posts) {
+    var matchingPost = posts.find(function(post) {
+      return _.lowerCase(req.params.postName) === _.lowerCase(post.title);
+    });
+    if (matchingPost) {
+      res.render("post", {
+        postTitle: matchingPost.title,
+        postBody: matchingPost.content
+      });
+    } else {
+      // If no matching post was found, render a 404 page or redirect to a search page.
+      res.status(404).send("Post not found");
     }
-  })
-
-
+  }).catch(function(err) {
+    console.log(err);
+    // Handle the error by rendering an error page or redirecting to an error page.
+  });
 });
 
+
+
 app.post("/compose", function(req,res){
-  const post = {
-    title:  req.body.postTitle,
-    content:  req.body.postBody
-  };
-  posts.push(post);
-  res.redirect("/");
-})
+  Post.find({}).then(function(posts) {
+    var matchingPost = posts.find(function(post) {
+      return req.body.postTitle === post.title;
+    });
+    if(matchingPost){
+      console.log("Already created");
+      // alert("Already created");
+    }else{
+      const post = new Post({
+        title:  req.body.postTitle,
+        content:  req.body.postBody
+      });
+      post.save();
+      res.redirect("/");
+    }
+  }).catch(function(err) {
+    console.log(err);
+    // Handle the error by rendering an error page or redirecting to an error page.
+  });
+});
+
 
 
 app.listen(3000, function() {
